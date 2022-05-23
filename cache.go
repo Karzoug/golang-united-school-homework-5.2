564d6bb034a050404f50fe203b06a0b73d5735bf
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-05-23 21:58:47 +0300

    implemented a simple cache

--- cache.go
+++ cache.go
@@ -2,22 +2,52 @@ package cache
 
 import "time"
 
+var maxTime = time.Unix(1<<63-62135596801, 999999999)
+
 type Cache struct {
+	items map[string]CacheItem
+}
+
+type CacheItem struct {
+	value       string
+	timeExpired time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{items: map[string]CacheItem{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	item, exists := c.items[key]
+	if !exists {
+		return "", false
+	}
 
+	if time.Now().After(item.timeExpired) {
+		delete(c.items, key)
+		return "", false
+	}
+
+	return item.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.items[key] = CacheItem{value: value, timeExpired: maxTime}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	ret := make([]string, 0, len(c.items))
+	for k, v := range c.items {
+		if time.Now().After(v.timeExpired) {
+			delete(c.items, k)
+		} else {
+			ret = append(ret, v.value)
+		}
+	}
+
+	return ret
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.items[key] = CacheItem{value: value, timeExpired: deadline}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module solution-5.2
+
+go 1.18
